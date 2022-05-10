e99ad8f9f183016de67c01d1bef7e10c16fb3cee
Max_On <malahovskijm02@mail.ru>
2022-05-10 15:45:35 +0300

    first_task_opros

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 'use srtict';
 
+const numberOfFilmes = +prompt("Сколько фильмов вы уже псмотрели ?","");
 
+const personalMovieBD = {
+    count:numberOfFilmes,
+    movies: {},
+    actors:{},
+    genres: [],
+    private: false
+};
+const a = prompt ("Один из последних просмотренных?" , " "),
+      b = prompt ("На сколько оцените его?" , " "),
+	  c = prompt ("Один из последних просмотренных?" , " "),
+	  d = prompt ("На сколько оцените его?" , " ");
+personalMovieBD.movies[a]=b;
+personalMovieBD.movies[c]=d;
+console.log(personalMovieBD);
 
